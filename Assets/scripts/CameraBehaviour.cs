d283975749aff223f07446c1d73fb6f578a6f70c
Leonardo Camilo Ribeiro <leo_lcr@hotmail.com>
2018-07-26 02:01:37 -0300

    enviroment good enough

---
+++ Assets/scripts/CameraBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+    void FixedUpdate()
+    {
+        var desiredPosition = target.position + offset;
+        var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+        transform.LookAt(target);
+    }
+}
